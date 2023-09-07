bc101d5b476a80664fdc2f8b3c961b38e256ff38
Andy Biira <biiraledum@gmail.com>
2023-09-06 18:32:48 -0700

    moved laravel files into repository

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <title>ANTHONI-ANEX-TONI.IO</title>
+   
+</head>
+<body>
+<header>
+   <div class="container">
+     <nav class="nav-list nav-item">
+     <a href="#">INFO</a></li>
+     <a href="#">BOOKS</a></li>
+     <a href="#">MEDICALS</a></li>
+     <a href="#">TEACHINGS</a></li>
+     <a href="#">CONTACT US</a></li>
+</div>
+</header>
+
+<section class="profile-section">
+    <div class="container">
+        <div class="profile-content">
+            <div class="profile-image">
+                <img src="/images/Anex-Toni.jpg" alt="Anex-Toni">
+            </div>
+         <div class="profile-description">
+            <h2> ANTHONI-ANEX-TONI </h2>
+          <p>Anthony Anex-Toni Asekhome hails from Erhunun Uneme in the Southern part of Nigeria, he is a master Story teller, Teacher and physician. He is with a vision to teach, care and nurture humans, in the highest beauty of life. As an educationists, he pioneers the establishment of educational institutions at all levels in Nigeria, including Exploit Learn Institute, where he serves as coordinator and Anionted Children School where he also serves as a teacher. He is also a Physician with a zeal to care for the sick. He has written over fifty texts covering various aspects of life.</p>
+        </div>
+        </div>
+    </div>
+     </section>
+    
+    <!-- ... rest of the HTML content ... -->
+
+    <footer>
+        <p>&copy; <?php echo date('Y'); ?> Your Website. All rights reserved.</p>
+    </footer>
+</body>
+</html>
